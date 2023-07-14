df424fdfde51ef160d0d63b62fe444a9906d7206
FaustinaTiara <ubakafaustina@gmail.com>
2023-07-14 16:14:36 -0700

    last D

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,8 +17,8 @@ int main(void)
 
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 19);
+		printf("Last digit of %d is %d and is grater than 5\n",
+			n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
